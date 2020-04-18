b03d5caaf54e211492ec46aa2251a47b05c94785
aawheatley <aa.wheatley97@gmail.com>
2020-04-18 10:30:47 -0400

    Update check-ip.py script to only display alert when ping fails

--- check-ip.py
+++ check-ip.py
@@ -8,20 +8,18 @@ def check(address):
 			ip=address
 			result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2", ip],
 			stdout=limbo, stderr=limbo).wait()
-			if result == 0:
-				print(ip, "is active")
-			elif result == 2:
-				print(ip, "is not active")
-			else:
-				print("Result was: ", result)
+			if result != 0:
+				print(ip, "is NOT active")
+			#else:
+			#	print(ip, "is active")
 	except Exception as e:
 		print("An error ocurred: ")
 		print(error)
 
 address = input("Enter the IP/host you want to check: ")
-verify = input("Are you sure you want to check %s ? (Y/n) " % str(address)).lower()
-if verify != 'y':
-	sys.exit()
+#verify = input("Are you sure you want to check %s ? (Y/n) " % str(address)).lower()
+#if verify != 'y':
+#	sys.exit()
 
 rest = int(input("How long do you want to wait until the next check? (between 0 and 60) "))
 if rest < 0 or rest > 60:
@@ -38,6 +36,7 @@ if repeat < 4 or repeat > 1000:
 	print("Try again and use a number between 4 and 1000")
 	sys.exit()
 elif repeat >= 4 and repeat <= 1000:
+	print("\nYou will not see any results until the host or IP entered stops responding.")
 	for i in range(0,repeat+1):
 		x += 1
 		if x == repeat:
@@ -49,6 +48,7 @@ elif repeat >= 4 and repeat <= 1000:
 else:
 	repeat = 200
 	x = 0
+	print("\nYou will not see any results until the host or IP entered stops responding.")
 	for i in range(0,repeat):
 		x += 1
 		if x == repeat:
