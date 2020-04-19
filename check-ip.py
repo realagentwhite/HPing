5582ee0acc22fefc1e54c33ef36372903094c565
aawheatley <aa.wheatley97@gmail.com>
2020-04-19 06:25:10 -0400

    Improve the function of the code

--- check-ip.py
+++ check-ip.py
@@ -2,58 +2,99 @@ import os, sys
 from time import sleep
 import subprocess
 
-def check(address):
+# Give some color to the texts
+class bcolors:
+	GREEN = '\033[92m'
+	YELLOW = '\033[93m'
+	RED = '\033[91m'
+	BOLD = '\033[1m'
+	ENDC = '\033[0m'
+
+
+
+def banner():
+	pipinger = '''
+ 888888ba  oo  888888ba  oo                                     
+ 88    `8b     88    `8b                                        
+a88aaaa8P' dP a88aaaa8P' dP 88d888b. .d8888b. .d8888b. 88d888b. 
+ 88        88  88        88 88'  `88 88'  `88 88ooood8 88'  `88 
+ 88        88  88        88 88    88 88.  .88 88.  ... 88       
+ dP        dP  dP        dP dP    dP `8888P88 `88888P' dP       
+oooooooooooooooooooooooooooooooooooooo~~~~.88~oooooooooooooooooo
+                                      d8888P                    
+	'''
+	return pipinger
+
+def check(address, get_alerts):
 	try:
 		with open(os.devnull, "wb") as limbo:
 			ip=address
 			result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2", ip],
 			stdout=limbo, stderr=limbo).wait()
 			if result != 0:
-				print(ip, "is NOT active")
-			#else:
-			#	print(ip, "is active")
-	except Exception as e:
+				print(ip + bcolors.RED + bcolors.BOLD + " is not active"+ bcolors.ENDC)
+			if result == 0 and get_alerts == True:
+				print(ip + bcolors.GREEN + bcolors.BOLD + " is active"+ bcolors.ENDC)
+	except Exception as error:
 		print("An error ocurred: ")
 		print(error)
+		input("Press enter to continue...")
+		main()
 
-address = input("Enter the IP/host you want to check: ")
-#verify = input("Are you sure you want to check %s ? (Y/n) " % str(address)).lower()
-#if verify != 'y':
-#	sys.exit()
-
-rest = int(input("How long do you want to wait until the next check? (between 0 and 60) "))
-if rest < 0 or rest > 60:
-	print("Enter a number between 0 and 60")
-	sys.exit()
-#if len(rest) == 0:
-#	rest = 30
-
-repeat = int(input("How many times would you like to check? (between 4 and 1000, default is 200) "))
-x = 0
-
-if repeat < 4 or repeat > 1000:
-	print("You entered %s which is not a valid input" % str(repeat))
-	print("Try again and use a number between 4 and 1000")
-	sys.exit()
-elif repeat >= 4 and repeat <= 1000:
-	print("\nYou will not see any results until the host or IP entered stops responding.")
-	for i in range(0,repeat+1):
-		x += 1
-		if x == repeat:
-			print("Done checking.\nRun the script again to check another host/ip")
+def main():
+	try:
+		os.system("clear")
+		print(bcolors.YELLOW + bcolors.BOLD + banner() + bcolors.ENDC)
+		address = input("Enter the IP/host you want to check: ")
+		rest = int(input("How long do you want to wait until the next check? (between 0 and 60) "))
+		if rest < 0 or rest > 60:
+			print("Enter a number between 0 and 60")
 			sys.exit()
+
+		repeat = int(input("How many times would you like to check? (greater than 4, default is 200) "))
+		x = 0
+		
+		alerts = input("Would you like to get alert if the host/IP is responding to pings? (Y/n) ").lower()
+		if alerts == 'y':
+			get_alerts = True
 		else:
-			check(address)
-			sleep(rest)
-else:
-	repeat = 200
-	x = 0
-	print("\nYou will not see any results until the host or IP entered stops responding.")
-	for i in range(0,repeat):
-		x += 1
-		if x == repeat:
-			print("Done checking.\nRun the script again to check another host/ip")
+			get_alerts = False
+		
+		if repeat < 4:
+			print("You entered %s which is not a valid input" % str(repeat))
+			print("Try again and use a number between 4 and 1000")
 			sys.exit()
+		elif repeat >= 4 and repeat <= 1000:
+			if get_alerts == False:
+				print("\nYou will not see any results until the host or IP entered stops responding.")
+
+			for i in range(0,repeat+1):
+				x += 1
+				if x == repeat+1:
+					print("Done checking.\nRun the script again to check another host/ip")
+				else:
+					check(address, get_alerts)
+					sleep(rest)
 		else:
-			check(address)
-			sleep(rest)
+			repeat = 200
+			x = 0
+			if get_alerts == False:
+				print("\nYou will not see any results until the host or IP entered stops responding.")
+
+			for i in range(0,repeat+1):
+				x += 1
+				if x == repeat:
+					print("Done checking.\nRun the script again to check another host/ip")
+				else:
+					check(address, get_alerts)
+					sleep(rest)
+
+	except KeyboardInterrupt:
+		print("Caught ctrl+c\nExiting now...")
+		sys.exit()
+	except Exception as error:
+		print("*"*20)
+		print(error)
+		print("*"*20)
+
+main()
